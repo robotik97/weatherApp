f7458d4e507232d9d9fca547848ed68fadce4a6f
unknown <edhar97@gmail.com>
2023-09-20 23:38:24 +0300

    refactor: change structure of project

--- weatherApp/src/App.vue
+++ weatherApp/src/App.vue
@@ -1,25 +1,32 @@
 <script>
-export default{
-  data (){
-    return{
+export default {
+  data() {
+    return {
       city: ""
     }
+  },
+  computed:{
+cityName(){
+return "«"+this.city+"»"
+}
   }
 }
 </script>
 
 <template>
-<div class="wrapper">
-<h1>weater app</h1>
-<p>find out the weather in {{city==""?"your city": city}}</p>
-<input type="text" v-model="city" placeholder="enter city">
-<button v-if="city!==''">push here</button>
-<button v-else="city==''">enter the name of the city</button>
-</div>
+  <div class="wrapper">
+    <h1>weater app</h1>
+    <p>find out the weather in {{ city == "" ? "your city" : cityName }}</p>
+    <input type="text" v-model="city" placeholder="enter city">
+    <!-- changing button -->
+    <!-- if the input is empty, then the button does not work -->
+    <button v-if="city !== ''">push here</button>
+    <button disabled v-else="city==''">enter the name of the city</button>
+  </div>
 </template>
 
 <style scoped>
-.wrapper{
+.wrapper {
   width: 400px;
   height: 250px;
   border-radius: 50px;
@@ -29,27 +36,34 @@ export default{
   text-align: center;
   margin-top: 50px;
 }
-.wrapper button{
+
+.wrapper button {
   margin: 10px;
-  background:rgb(38,38,39);
+  background: rgb(38, 38, 39);
   border-radius: 25px;
   padding: 5px 10px;
   outline: none;
   color: #797b74;
 }
-.wrapper input{
+
+.wrapper input {
   background: #74e70e;
   border: none;
   border-radius: 25px;
   padding: 5px 10px;
   font-size: 14px;
 }
-.wrapper input:focus{
-  border-bottom-color: rgb(38,38,39);
+
+.wrapper button:disabled {
+  background:  rgb(73, 73, 74);
+}
+
+.wrapper input:focus {
+  border-bottom-color: rgb(38, 38, 39);
   border: none;
 }
- .wrapper button:hover{
-  transform: scale(1.1) translateY(-5px);
- }
 
+.wrapper button:hover {
+  transform: scale(1.1) translateY(-5px);
+}
 </style>
