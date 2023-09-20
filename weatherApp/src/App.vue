e6f3fb60781df3b42b69e45ed1b28b6aade4e0ad
unknown <edhar97@gmail.com>
2023-09-20 22:08:30 +0300

    init:start project

---
+++ weatherApp/src/App.vue
@@ -0,0 +1,53 @@
+<script>
+export default{
+  data (){
+    return{
+      city: ""
+    }
+  }
+}
+</script>
+
+<template>
+<div class="wrapper">
+<h1>weater app</h1>
+<p>find out the weather in the {{city}}</p>
+<input type="text" v-model="city" placeholder="enter city">
+<button>push here</button>
+</div>
+</template>
+
+<style scoped>
+.wrapper{
+  width: 400px;
+  height: 250px;
+  border-radius: 50px;
+  color: #4c4e4a;
+  padding: 20px;
+  background: #74e70e;
+  text-align: center;
+  margin-top: 50px;
+}
+.wrapper button{
+  margin: 10px;
+  background:rgb(38,38,39);
+  border-radius: 25px;
+  padding: 5px 10px;
+  outline: none;
+  color: #797b74;
+}
+.wrapper input{
+  background: #74e70e;
+  border: none;
+  border-radius: 25px;
+  padding: 5px 10px;
+  font-size: 14px;
+}
+.wrapper input:focus{
+  border-bottom-color: rgb(237, 255, 215);
+}
+ .wrapper button:hover{
+  transform: scale(1.1) translateY(-5px);
+ }
+
+</style>
